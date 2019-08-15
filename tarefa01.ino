58070c38db9d28c60de8d4875e5face3cb12c02e
Nobrinks <43222335+Nobrinks@users.noreply.github.com>
2019-08-14 21:40:25 -0300

    Update tarefa01.ino
    Concluída

--- tarefa01.ino
+++ tarefa01.ino
@@ -1,22 +1,14 @@
-#define LED_PIN 12
-#define BUT_PIN 2
-#define BUT2_PIN 4
+#define LED_PIN 2
+#define BUT_PIN 12
+#define BUT2_PIN 8
 byte status=0;
-int intervalo=1000;
-unsigned long tempo;
+long int tInicial=0, tFinal, intervalo=1000, tempo;
+boolean on_off=true;
 void setup() {
   // put your setup code here, to run once:
   pinMode(LED_PIN, OUTPUT);
   pinMode(BUT_PIN, INPUT);
   pinMode(BUT2_PIN, INPUT);
-  digitalWrite(BUT2_PIN,HIGH);
-  digitalWrite(BUT_PIN,HIGH);
-}
-void pisca(){
-  digitalWrite(LED_PIN, HIGH);
-  delay(intervalo);
-  digitalWrite(LED_PIN, LOW);
-  delay(intervalo);
 }
 void frequencia(){
   if(status==1){
@@ -29,25 +21,37 @@ void frequencia(){
     digitalWrite(LED_PIN, LOW);
   }
 }
-
+void pisca(){
+  tempo=millis();
+  if(on_off==true){
+    if((tempo-tInicial)>=intervalo){
+      digitalWrite(LED_PIN, HIGH); tInicial=tempo; on_off=false;
+    }
+  }
+  else{
+    if((tempo-tInicial)>=intervalo){
+      tInicial=tempo; digitalWrite(LED_PIN, LOW); on_off=true;
+     }
+  }
+}
 void loop() {
   // put your main code here, to run repeatedly:
-  tempo=millis();
   pisca();
   if( (digitalRead(BUT2_PIN)==0) && (digitalRead(BUT_PIN)==0) ){
     status=3;
     frequencia();
+    status=0;
     while(1);
   }
   if(digitalRead(BUT_PIN)==LOW){
     status=1;
     frequencia(); 
-    while(digitalRead(BUT_PIN)==LOW){pisca();}
+    while(digitalRead(BUT_PIN)==LOW){pisca(); status=0;}  
   }
   if(digitalRead(BUT2_PIN)==LOW){
     status=2;
     frequencia(); 
-    while(digitalRead(BUT2_PIN)==LOW){pisca();}
+    while(digitalRead(BUT2_PIN)==LOW){pisca(); status=0;}
   }
    
 }
